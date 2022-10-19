fe384d56e581cf56b84fdc928f9ce96956695679
Przemyslaw Gogolewski <przemyslaw.gogolewski.1978@gmail.com>
2022-10-19 08:54:58 +0200

    initializing graph window

--- DesktopProject/QT/Kalorymetr/kalorymetr/mainwindow.cpp
+++ DesktopProject/QT/Kalorymetr/kalorymetr/mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->mainPlot->setInteraction(QCP::iRangeDrag, true);
+    ui->mainPlot->setInteraction(QCP::iRangeZoom, true);
+    ui->mainPlot->addGraph();
 }
 
 MainWindow::~MainWindow()
