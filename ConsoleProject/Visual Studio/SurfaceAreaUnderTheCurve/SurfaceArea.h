30672ade09e9de1717a0caf2aa6d03dc81402e2a
Przemyslaw Gogolewski <przemyslaw.gogolewski.1978@gmail.com>
2022-10-28 12:43:10 +0200

    New class Surface under the Curve

---
+++ ConsoleProject/Visual Studio/SurfaceAreaUnderTheCurve/SurfaceArea.h
@@ -0,0 +1,7 @@
+#pragma once
+class SurfaceArea
+{
+public:
+
+};
+
---
+++ ConsoleProject/Visual Studio/SurfaceAreaUnderTheCurve/SurfaceAreaUnderTheCurve.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include "SurfaceAreaUnderTheCurve.h"
+
+int main()
+{
+    std::vector<double> dX = { 285.000, 285.015, 285.030, 285.045, 285.060, 285.075, 285.090 };
+    std::vector<double> dY = { 33.082, 32.457, 32.412, 33.206, 33.052, 33.585, 33.194 };
+
+}
+
